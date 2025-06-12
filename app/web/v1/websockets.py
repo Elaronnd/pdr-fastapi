1b4a03bdd65a434837b0016433fbed8f0e738648
D1ther <maxnozhkin2006@gmail.com>
2025-06-12 19:05:55 +0300

    додав чат

--- app/web/v1/websockets.py
+++ app/web/v1/websockets.py
@@ -1,4 +1,54 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from typing import Set
 
+from app.schemas.pydantic_users import (
+    UserData
+)
 
-websocket_router = APIRouter()
+from app.db.queries import (
+    get_user_by_id
+)
+
+websocket_router = APIRouter(prefix="/ws")
+
+active_users: Set[WebSocket] = set()
+
+@websocket_router.websocket("/chat")
+async def chat_websocket(websocket: WebSocket):
+    await websocket.accept()
+
+    id = websocket.query_params["id"]
+
+    if not id:
+        await websocket.close(code=1008, reason="User ID not provided")
+        raise ValueError("User ID not provided")
+
+    user = get_user_by_id(id)
+
+    if not user:
+        await websocket.close(code=1008, reason="User not found")
+        raise ValueError("User not found")
+    
+    name = user["username"]
+    active_users.add(websocket)
+    
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            for user in active_users.copy(): 
+                if websocket != user:
+                    try:
+                        await user.send_text(f"{name}: {data}")
+                    except:
+                        active_users.discard(user)
+
+    except WebSocketDisconnect:
+        pass
+
+    except Exception as e:
+        print(f"WebSocket error: {e}")
+
+    finally:
+        await websocket.close()
+        active_users.discard(websocket)
